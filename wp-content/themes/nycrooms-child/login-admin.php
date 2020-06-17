f1f05cc57a68b693200e520a14ec91a7ba25513f
git config --global user.name ee <midriffdeveloper6@gmail.com>
2020-06-17 15:39:45 +0530

    redirect admin

--- wp-content/themes/nycrooms-child/login-admin.php
+++ wp-content/themes/nycrooms-child/login-admin.php
@@ -41,7 +41,7 @@ if(isset($_POST['login'])){
 			$loginerror = "Invalid login details";  
 		   // Note, I have created a page called "Error" that is a child of the login page to handle errors. This can be anything, but it seemed a good way to me to handle errors.  
 		 } else {    
-		   echo "<script type='text/javascript'>window.location.href='". site_url().'/admin-dashboard/' ."'</script>";  
+		   echo "<script type='text/javascript'>window.location.href='". site_url().'/admin/' ."'</script>";  
 		   exit();  
 		 }
    }
